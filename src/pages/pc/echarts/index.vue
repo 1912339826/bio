30dea214fddac3bde04ed8138c07f5bbc278ffc7
username@email.com <1912339826@qq.com>
2020-07-09 18:37:01 +0800

    2020年7月9日18:36:58

--- src/pages/pc/echarts/index.vue
+++ src/pages/pc/echarts/index.vue
@@ -1,84 +1,38 @@
 <template>
   <div id="echarts">
-    <div class="children SmoothedLineChart">
+    <div class="children LineChart">
       <nav>
         <img src="static/images/chevron.png" alt />
-        <span>平滑折线图</span>
+        <span>折线图</span>
       </nav>
       <section>
-        <div class="box">
-          <componentsSmoothedLineChart
-            :lineStyle="SmoothedLineChart"
-            ref="componentsSmoothedLineCharts"
-          />
-        </div>
-        <div class="content_right">
-          <p>
-            <img src="static/images/chevron.png" alt />
-            <span>lineStyle:</span>
-          </p>
-          <p>
-            线性渐变:
-            <label for="lineStyle_begin_color">0%:</label>
-            <input
-              type="color"
-              name
-              id="lineStyle_begin_color"
-              :default-value="SmoothedLineChart.lineStyle_begin_color"
-              @change="lineStyle_begin_color($event)"
-            />
-            <label for="lineStyle_end_color">100%:</label>
-            <input
-              type="color"
-              name
-              id="lineStyle_end_color"
-              :default-value="SmoothedLineChart.lineStyle_end_color"
-              @change="lineStyle_end_color($event)"
-            />
-          </p>
-          <p>
-            <a-button type="link" @click="confirm">Link</a-button>
-          </p>
-        </div>
+        <componentsLineChart />
       </section>
+    </div>
+    <div class="children">
     </div>
   </div>
 </template>
 
 <script>
-import componentsSmoothedLineChart from "../../../components/echarts/SmoothedLineChart";
+import componentsLineChart from "../../../components/echarts/LineChart";
+import shiyan from '../../../components/shiyan'
 export default {
   name: "echarts",
   components: {
-    componentsSmoothedLineChart
+    componentsLineChart,
+    shiyan
   },
   props: {},
   data() {
-    return {
-      SmoothedLineChart: {
-        lineStyle_begin_color: "#b53030",
-        lineStyle_end_color: "#6f5858"
-      }
-    };
+    return {};
   },
-  beforeUpdate() {
-    // console.log(this.$refs.componentsSmoothedLineCharts);
-  },
+  beforeUpdate() {},
   created() {},
   mounted() {},
   activated() {},
   update() {},
-  methods: {
-    confirm() {
-      console.log(this.SmoothedLineChart);
-    },
-    lineStyle_begin_color(e) {
-      this.SmoothedLineChart.lineStyle_begin_color = e.target.value;
-    },
-    lineStyle_end_color(e) {
-      this.SmoothedLineChart.lineStyle_end_color = e.target.value;
-    }
-  },
+  methods: {},
   filters: {},
   computed: {},
   watch: {}
@@ -97,7 +51,7 @@ export default {
       display: flex;
       align-items: center;
       img {
-        height: 2vw;
+        height: 1.5vw;
       }
       span {
         display: block;
@@ -107,22 +61,13 @@ export default {
       }
     }
     section {
-      display: flex;
-      justify-content: space-between;
-      .box {
-        height: 20vw;
-        width: 50%;
-      }
-      .content_right {
-        width: 50%;
-        border-left: 2px solid #bdb6ad;
-        background-color: #fff6e9;
-        padding: 1vw;
-      }
+      height: 20vw;
+      width: 100%;
     }
   }
-  .SmoothedLineChart {
+  .LineChart {
     section {
+      width: 100%;
       height: 20vw;
     }
   }
